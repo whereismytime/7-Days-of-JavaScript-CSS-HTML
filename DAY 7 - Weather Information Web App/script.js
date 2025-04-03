f275bbc1c067490653d47eb2ee2a9920a7c1f15f
whereismytime <a.v.dyma@gmail.com>
2025-04-03 13:55:29 +0100

    Update Weather

--- DAY 7 - Weather Information Web App/script.js
+++ DAY 7 - Weather Information Web App/script.js
@@ -3,18 +3,14 @@ const form = document.querySelector(".weather-form");
 const input = document.querySelector(".input");
 const output = document.querySelector(".weather-output");
 
-form.onsubmit = async function (e) {
+form.onsubmit = async function(e) {
   e.preventDefault();
   const city = input.value.trim();
   if (!city) return;
-
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-
   const res = await fetch(url);
   const data = await res.json();
-
   output.innerHTML = "";
-
   if (data.error) {
     const html = `<div class="card"><p>${data.error.message}</p></div>`;
     output.insertAdjacentHTML("beforeend", html);
@@ -23,12 +19,11 @@ form.onsubmit = async function (e) {
       <div class="card">
         <h2 class="card-city">${data.location.name}, <span>${data.location.country}</span></h2>
         <div class="card-weather">
-          <div class="card-value">${data.current.temp_c} <sup>°</sup>C</div>
+          <div class="card-value">${data.current.temp_c}<sup>°</sup>C</div>
           <img src="pogoda.png" alt="weather icon" class="img-w" />
         </div>
         <div class="description">${data.current.condition.text}</div>
-      </div>
-    `;
+      </div>`;
     output.insertAdjacentHTML("beforeend", html);
   }
 };
